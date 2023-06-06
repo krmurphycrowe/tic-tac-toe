dd5a11e99b843e58f9630a0fac43bac085732728
krmurphycrowe <kmurphycrowe@gmail.com>
2023-06-06 20:09:25 +0100

    first commit, following tutorial: https://www.youtube.com/watch?v=3ZSclLOL0QA

--- tic-tac-toe/ContentView.swift
+++ tic-tac-toe/ContentView.swift
@@ -7,15 +7,46 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+enum SquareStatus {
+    case empty
+    case x // user 1
+    case o // user 2
+}
+
+class Square : ObservableObject {
+    @Published var sqStatus : SquareStatus
+    init(status:SquareStatus) {
+        self.sqStatus = status
+    }
+}
+
+class TTTModel : ObservableObject {
+    @Published var squares = [Square]()
+    
+    init(){
+        for _ in 0...8 {
+            squares.append(Square(status: .empty))
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    @StateObject var tttModel = TTTModel()
+    
+    var body: some View {
+        VStack{
+            
+            ForEach(0 ..< Int(tttModel.squares.count / 3), content: {
+                row in
+                HStack {
+                    ForEach(0 ..< 3, content: {
+                        column in
+                        Color.gray.frame(width:70, height:70, alignment: .center)
+                    })
+                }
+            })
+            
+        }
     }
 }
 
