b06a2432b19b50666803fa88346344b43d4f1d5d
krmurphycrowe <kmurphycrowe@gmail.com>
2023-06-06 22:42:08 +0100

    Added some more functionality, diverging from tutorial slightly in that my app will be 2 player (to do: finish the win check + restart)

--- tic-tac-toe/ContentView.swift
+++ tic-tac-toe/ContentView.swift
@@ -26,6 +26,8 @@ class Square : ObservableObject {
 
 class TTTModel : ObservableObject {
     @Published var squares = [Square]()
+    @Published var currentPlayer : Int = 0
+    @Published var currentPlayerText : String = "Player X's Turn"
     
     init(){
         for _ in 0...(num_sq-1) {
@@ -36,6 +38,22 @@ class TTTModel : ObservableObject {
     func resetGame() {
         for _ in 0...(num_sq-1) {
             squares.append(Square(status: .empty))
+        }
+    }
+    
+    var gameOver : (SquareStatus, Bool) {
+        get{
+            if yesWinner != .empty {
+                return (yesWinner, true)
+            } else {
+                for i in 0...(num_sq-1) {
+                    if squares[i].sqStatus == .empty {
+                        return (.empty, false)
+                    }
+                }
+            }
+            
+            return (.empty, true)
         }
     }
     
@@ -75,9 +93,17 @@ class TTTModel : ObservableObject {
         return nil
     }
     
-    func makeMove(index: Int, player: SquareStatus) -> Bool {
+    func makeMove(index: Int) -> Bool {
         if squares[index].sqStatus == .empty {
-            squares[index].sqStatus = player
+            if self.currentPlayer == 0 {
+                squares[index].sqStatus = .x
+                self.currentPlayerText = "Player O's Turn"
+            } else {
+                squares[index].sqStatus = .o
+                self.currentPlayerText = "Player X's Turn"
+            }
+            
+            self.currentPlayer = (self.currentPlayer + 1) % 2
             //if player == .x {
                 // do ai move for o if playing against ai
             //}
@@ -94,7 +120,7 @@ struct SquareView : View {
     
     var body: some View {
         Button(action: {
-            
+            self.action()
         }, label: {
             Text(self.dataSource.sqStatus == .x ? "X" : self.dataSource.sqStatus == .o ? "O" : " ")
                 .font(.largeTitle)
@@ -110,10 +136,17 @@ struct SquareView : View {
 struct ContentView: View {
     @StateObject var tttModel = TTTModel()
     
-    func buttonAction(_ index : Int) {}
+    func buttonAction(_ index : Int) {
+        _ = self.tttModel.makeMove(index: index)
+    }
     
     var body: some View {
         VStack{
+            Text("Tic Tac Toe")
+                .bold()
+                .foregroundColor(Color.black.opacity(0.7))
+                .padding(.bottom)
+                .font(.title2)
             
             ForEach(0 ..< row_col_sz, id: \.self, content: {
                 row in
@@ -126,6 +159,11 @@ struct ContentView: View {
                 }
             })
             
+            Text(tttModel.currentPlayerText)
+                .bold()
+                .foregroundColor(Color.black.opacity(0.7))
+                .padding(.bottom)
+                .font(.title2)
         }
     }
 }
